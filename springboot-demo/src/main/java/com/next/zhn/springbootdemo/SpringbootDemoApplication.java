5b936bc7fe660709860a1ae617e57d6b3beca218
zhn <944970483@qq.com>
2019-11-27 19:49:42 +0800

    first commit

---
+++ springboot-demo/src/main/java/com/next/zhn/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.next.zhn.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
